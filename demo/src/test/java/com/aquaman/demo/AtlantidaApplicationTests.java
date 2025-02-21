7b3c32c0a667d03590a7e00ccde94e643dc35968
LauMoser <laura.moser@germinare.org.br>
2025-02-21 08:57:27 -0300

    Projeto Spring
    Linguagem: Java

---
+++ demo/src/test/java/com/aquaman/demo/AtlantidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquaman.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlantidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
