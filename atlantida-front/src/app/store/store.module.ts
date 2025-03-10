486d532f3c7eee0939008f77712baac28a6edd79
Braz <enzo.braz@germinare.org.br>
2025-03-10 13:08:05 -0300

    Criando o component product options e comecando o component do filtro

--- atlantida-front/src/app/store/store.module.ts
+++ atlantida-front/src/app/store/store.module.ts
@@ -1,22 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { StoreRoutingModule } from './store-routing.module';
 import { StoreComponent } from './store.component';
-import { StoreProductOptionsModule } from './components/product-options/product-options.module';
-import { HeaderModule } from '../components/header/header.module';
-import { StoreHeaderModule } from './components/header/header.module';
 
 @NgModule({
-  declarations: [
-    StoreComponent,
-  ],
-  imports: [
-    BrowserModule,
-    StoreRoutingModule,
-    StoreProductOptionsModule,
-    StoreHeaderModule
-  ],
+  declarations: [StoreComponent,],
+  imports: [],
   providers: [],
-  bootstrap: [StoreComponent]
 })
 export class StoreModule { }
