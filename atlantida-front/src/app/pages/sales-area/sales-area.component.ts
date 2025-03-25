cb69f4dbc098fd89cf1ff9b9ba2850bdd19d3811
Braz <enzo.braz@germinare.org.br>
2025-03-25 13:21:41 -0300

    Criando a area do administrador

---
+++ atlantida-front/src/app/pages/sales-area/sales-area.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'atlantida-admin-space-sales-area',
+  templateUrl: './sales-area.component.html',
+  styleUrl: './sales-area.component.css'
+})
+export class AdminSpaceSalesAreaComponent {
+}
---
+++ atlantida-front/src/app/pages/sales-area/sales-area.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { AdminSpaceSalesAreaComponent } from './sales-area.component';
+
+@NgModule({
+  declarations: [AdminSpaceSalesAreaComponent],
+  imports: [],
+  providers: [],
+})
+export class AdminSpaceSalesAreaModule { }
